f12c5fb22300b651abaa5b041ad54e2e781e2531
luispedro <luispribeiro9@gmail.com>
2022-11-13 17:13:39 +0000

    updating Addmnanagement script to add host to DVS and migrate vmkernel adapter to DVS

--- vSphere/AddManagementDVS.ps1
+++ vSphere/AddManagementDVS.ps1
@@ -2,27 +2,51 @@ param(
 $dvsname,
 $cluster,
 $nic1,
-$nic2
+$nic2,
+$management_pg,
+[bool]$teaming_policy =$true
 )
 
-#add to service dvswitch
+#get all the hosts in the cluster
 $servers = get-cluster -name $cluster |get-vmhost
 
-#$dvsname = "dvSwitch-Internal"
+# get the distributeed switch and creates the vmnics array
 $dvs = Get-VDSwitch -Name $dvsname
-$nics = @()
-$nics += $nic1
-$nics += $nic2
+$pnics = @()
+$success_added_host = $false
 
+
+# for each esxi, add to DVS and migrates the management vmkernel on standard switch to DVS switch for the specified distributed port group for management
 foreach ($server in $servers){
-	$dvs | Add-VDSwitchVMHost -VMHost $server
-	#Get-VMHostNetworkAdapter -VMHost $server -Portgroup "Management"
-	$vmk0 = Get-VMHostNetworkAdapter -VMHost $server -Portgroup "Management Network"
-	$management_vd_pg = Get-VDPortGroup -Name "Management-40" -VDSwitch $vds
-	Add-VDSwitchPhysicalNetworkAdapter -DistributedSwitch $dvs -VMHostPhysicalNic $nics -VMHostVirtualNic $vmk0 -VirtualNicPortgroup $management_vd_pg
+	
+	try{
+		#get the physical nic based on name of input nics
+		$physicalNic1 = Get-VMHostNetworkAdapter -VMHost $server -Name $nic1
+		$physicalNic2 = Get-VMHostNetworkAdapter -VMHost $server -Name $nic2
+		$pnics += $physicalNic1
+		$pnics += $physicalNic2
+
+		#add host to DVS
+		$dvs | Add-VDSwitchVMHost -VMHost $server
+		
+		#migrate the vmkernel to DVS
+		$vmk0 = Get-VMHostNetworkAdapter -VMHost $server -Portgroup "Management Network"
+		$management_vd_pg = Get-VDPortGroup -Name $management_pg -VDSwitch $dvs			
+		Add-VDSwitchPhysicalNetworkAdapter -DistributedSwitch $dvs -VMHostPhysicalNic $pnics -VMHostVirtualNic $vmk0 -VirtualNicPortgroup $management_vd_pg
+		$success_added_host = $true
+	}	
+	catch {
+		write-host $_
+		Write-host "unable to get physical nics or add host to dvs or migrate the vmkernel"
+	}
+
 }
 
-Get-VDPortgroup -Name "Management-40" |Get-VDUplinkTeamingPolicy |Set-VDUplinkTeamingPolicy -ActiveUplinkPort "Uplink 1" -StandbyUplinkPort "Uplink 2"
+# if teaming policy is set to true, then changes the active/standby uplink configuration on the distributed port group used for management
+if ($teaming_policy -and $success_added_host){
+	Get-VDPortgroup -Name $management_pg |Get-VDUplinkTeamingPolicy |Set-VDUplinkTeamingPolicy -ActiveUplinkPort "Uplink 1" -StandbyUplinkPort "Uplink 2"	
+}
+
 
 
 
--- vSphere/CreateCluster.ps1
+++ vSphere/CreateCluster.ps1
@@ -1,3 +1,5 @@
+# creates a new cluster in the specified datacenter #
+
 param(
 	$datacenter,
 	$name
